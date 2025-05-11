4b007d98668f3eb8f6393d1a884c9fada3d4b56e
Xever17 <93504929+Xever17@users.noreply.github.com>
2025-05-12 00:51:27 +0500

    Add files via upload

---
+++ BilimedukzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilimedukz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilimedukzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
